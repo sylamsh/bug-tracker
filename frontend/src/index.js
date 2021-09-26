15d5840868a36695d6c35772f2470b3f87d0e1e4
Sylamsh <58618384+Sylamsh@users.noreply.github.com>
2021-09-27 02:09:27 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
@@ -9,21 +8,6 @@ import './index.css';
 import App from './App';
 
 import reducers from './Controllers/reducers'
-// // Reducers
-// import authReducer from './Controllers/Redux/authSlice'
-// import bugReducer from './Controllers/Redux/bugSlice'
-// import userReducer from './Controllers/Redux/userSlice'
-
-// // Redux Configure
-// const rootReducer = combineReducers({
-//   auth : authReducer,
-//   bugs : bugReducer,
-//   user : userReducer, 
-// })
-
-// const store = configureStore({
-//   reducer : rootReducer,
-// })
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
